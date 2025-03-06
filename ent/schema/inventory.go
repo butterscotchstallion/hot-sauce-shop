862c010b800cb29e6bb9cd98c43411763b7c9c36
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-03-06 15:58:38 -0500

    Adds Inventory schema

---
+++ ent/schema/inventory.go
@@ -0,0 +1,32 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Inventory holds the schema definition for the Inventory entity.
+type Inventory struct {
+	ent.Schema
+}
+
+// Fields of the Inventory.
+func (Inventory) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("name"),
+		field.String("description"),
+		field.String("shortDescription"),
+		field.Time("createdAt").
+			Default(time.Now),
+		field.Time("updatedAt").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+	}
+}
+
+// Edges of the Inventory.
+func (Inventory) Edges() []ent.Edge {
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module hotsauceshop
+
+go 1.24.1
+
+require entgo.io/ent v0.14.3
