bc867f1170c8f7e2f2f252bdfd2cc34bf8b0966c
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-03-07 00:16:29 -0500

    Adds inventory route

---
+++ server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"hotsauceshop/ent"
+)
+
+var client *ent.Client
+
+func initDB() {
+	var err error
+	client, err = ent.Open("postgres",
+		"host=localhost port=5432 sslmode=disable user=hotsauceboss"+
+			"dbname=hotsauceshop password='unshaken context deniable shabby jimmy plunging'")
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	log.Println("Connected to postgres")
+	defer func(client *ent.Client) {
+		err := client.Close()
+		if err != nil {
+			log.Fatalf("failed closing client: %v", err)
+		}
+	}(client)
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+	log.Println("database schema updated")
+}
+
+func main() {
+	initDB()
+
+	r := gin.Default()
+
+	r.GET("/inventory", func(c *gin.Context) {
+		res := gin.H{
+			"status":  "ERROR",
+			"message": "Error fetching inventory",
+		}
+		inventory, err := client.Inventory.Query().All(c)
+		if err != nil {
+			res = gin.H{
+				"status": "OK",
+				"results": gin.H{
+					"inventory": inventory,
+				},
+			}
+		}
+		c.JSON(200, res)
+	})
+
+	err := r.Run("localhost:8080")
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+}
