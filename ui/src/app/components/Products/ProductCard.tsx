bfa33e656342bf4ecbce83ea5c28e7364d088b28
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-03-07 22:54:37 -0500

    Adds Prime Icons

--- ui/src/app/components/Products/ProductCard.tsx
+++ ui/src/app/components/Products/ProductCard.tsx
@@ -1,5 +1,6 @@
 import {IProduct} from "./IProduct.ts";
 import {Card} from "primereact/card";
+import {Button} from "primereact/button";
 
 interface IProductCardProps {
     product: IProduct
@@ -14,7 +15,12 @@ export default function ProductCard(props: IProductCardProps) {
                 alt={props.product.shortDescription}
             /></Card>
             <Card title={props.product.name}>
-                <p>{props.product.shortDescription}</p>
+                <p className="pb-4">{props.product.shortDescription}</p>
+
+                <div className="flex justify-between">
+                    <span className="text-green-200 font-bold">${props.product.price.toFixed(2)}</span>
+                    <Button label="Add" icon="pi pi-shopping-cart" className="p-button-success"/>
+                </div>
             </Card>
         </div>
     )
--- ui/src/vite.config.ts
+++ ui/src/vite.config.ts
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
+import {defineConfig, searchForWorkspaceRoot} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+    server: {
+        fs: {
+            allow: [
+                // search up for workspace root
+                searchForWorkspaceRoot(process.cwd()),
+                // your custom rules
+                'E:/projects/hot-sauce-shop/ui/node_modules/.pnpm/primeicons@7.0.0/node_modules/primeicons/fonts/primeicons.ttf',
+                "E:/projects/hot-sauce-shop/ui/node_modules/.pnpm/primeicons@7.0.0/node_modules/primeicons/fonts/primeicons.woff",
+                "E:/projects/hot-sauce-shop/ui/node_modules/.pnpm/primeicons@7.0.0/node_modules/primeicons/fonts/primeicons.woff",
+                "E:/projects/hot-sauce-shop/ui/node_modules/.pnpm/primeicons@7.0.0/node_modules/primeicons/fonts/primeicons.woff2"
+            ],
+        },
+    },
+    plugins: [react(), tailwindcss()],
 })
