14d1fcb608a92ae95f9d9bbad55e675f8695c7b3
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-03-09 01:29:47 -0500

    Adds skeletons even though the page loads so fast I can't see them!

--- ui/src/app/components/Products/ProductList.tsx
+++ ui/src/app/components/Products/ProductList.tsx
@@ -1,6 +1,7 @@
 import {IProduct} from "./IProduct.ts";
 import {ReactElement} from "react";
 import ProductCard from "./ProductCard.tsx";
+import {Skeleton} from "primereact/skeleton";
 
 interface IProductListProps {
     products: IProduct[],
@@ -10,10 +11,13 @@ export default function ProductList(props: IProductListProps): ReactElement {
     const productsList: ReactElement[] = props.products?.map((product: IProduct, index: number): ReactElement => (
         <ProductCard product={product} key={index}/>
     ));
+    const skeletonList: ReactElement[] = new Array(10).fill(0).map((): ReactElement => (
+        <Skeleton size="260px"></Skeleton>
+    ));
     return (
         <>
             <section className="flex gap-4 flex-wrap">
-                {productsList}
+                {props.products ? productsList : skeletonList}
             </section>
         </>
     );
