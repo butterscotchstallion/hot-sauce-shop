4a48fc2aff1057985acc2e053dc258cf83070cc3
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-03-17 17:15:59 -0400

    Refactors tag filters into their own component

--- ui/src/app/components/Products/ProductFilterList.tsx
+++ ui/src/app/components/Products/ProductFilterList.tsx
@@ -1,22 +1,59 @@
-import {ReactElement} from "react";
+import * as React from "react";
+import {ReactElement, useEffect, useState} from "react";
 import {Checkbox, CheckboxChangeEvent} from "primereact/checkbox";
 import {IDisplayTag} from "../../pages/ProductListPage.tsx";
 import {ITag} from "../Tag/ITag.ts";
+import {Subscription} from "rxjs";
+import {getTags} from "../Tag/TagService.ts";
+import {Toast} from "primereact/toast";
 
 interface IProductFilterListProps {
-    tags: IDisplayTag[],
-    toggleFilter: (tag: ITag, checked: boolean) => void,
+    toast: React.RefObject<Toast | null>;
+    onFiltersChanged: (filters: IDisplayTag[]) => void;
 }
 
 export default function ProductFilterList(props: IProductFilterListProps): ReactElement {
+    const [tags, setTags] = useState<IDisplayTag[]>([]);
+
+    useEffect(() => {
+        const tags$: Subscription = getTags().subscribe({
+            next: (results: ITag[]) => {
+                const displayTags: IDisplayTag[] = [];
+                results.map((tag: ITag) => {
+                    displayTags.push({...tag, checked: false});
+                });
+                setTags(displayTags);
+            },
+            error: (err) => {
+                if (props.toast.current) {
+                    props.toast.current.show({
+                        severity: 'error',
+                        summary: 'Error',
+                        detail: 'Error loading filters: ' + err,
+                        life: 3000,
+                    })
+                }
+            }
+        });
+        return () => {
+            tags$.unsubscribe();
+        }
+    }, [props.toast]);
+
+    function toggleFilter(checked: boolean, index: number) {
+        tags[index].checked = checked;
+        setTags([...tags]);
+        props.onFiltersChanged(tags.filter((tag: IDisplayTag): boolean => tag.checked));
+    }
+
     return (
         <>
             <ul>
-                {props.tags.map((tag: IDisplayTag, index: number) => (
+                {tags.map((tag: IDisplayTag, index: number) => (
                     <li key={index}>
                         <Checkbox
                             inputId={"filter-" + tag.slug}
-                            onChange={(e: CheckboxChangeEvent) => props.toggleFilter(tag, !!e.checked)}
+                            onChange={(e: CheckboxChangeEvent) => toggleFilter(!!e.checked, index)}
                             checked={tag.checked}
                         />
                         <label htmlFor={"filter-" + tag.slug}
--- ui/src/app/pages/ProductListPage.tsx
+++ ui/src/app/pages/ProductListPage.tsx
@@ -1,17 +1,16 @@
 import {ReactElement, RefObject, SetStateAction, Suspense, useEffect, useRef, useState} from "react";
 import {Card} from "primereact/card";
-import ProductFilterList from "../components/Products/ProductFilterList.tsx";
 import {IProduct} from "../components/Products/IProduct.ts";
 import {Subscription} from "rxjs";
 import ProductList from "../components/Products/ProductList.tsx";
 import Throbber from "../components/Shared/Throbber.tsx";
 import {ITag} from "../components/Tag/ITag.ts";
-import {getTags} from "../components/Tag/TagService.ts";
 import {IProductsResults} from "../components/Products/IProductsResults.ts";
 import {Paginator} from "primereact/paginator";
 import {getProducts} from "../components/Products/ProductService.ts";
 import {Toast} from "primereact/toast";
 import {Dropdown} from "primereact/dropdown";
+import ProductFilterList from "../components/Products/ProductFilterList.tsx";
 
 // Tags with the checked attribute layered on top for
 // the purposes of this component
@@ -29,7 +28,6 @@ export default function ProductListPage(): ReactElement {
     const [offset, setOffset] = useState<number>(0);
     const [perPage, setPerPage] = useState<number>(10);
     const [products, setProducts] = useState<IProduct[]>([]);
-    const [tags, setTags] = useState<IDisplayTag[]>([]);
     const [totalProducts, setTotalProducts] = useState<number>(0);
     const [productListSortKey, setProductListSortKey] = useState<string>("name");
     const productListSortOptions: IProductListSortOptions[] = [
@@ -37,22 +35,19 @@ export default function ProductListPage(): ReactElement {
         {label: "Sort by Price", value: "price"},
         {label: "Sort by Spice Rating", value: "spice_rating"},
     ];
-
-    function toggleFilter(tag: ITag, checked: boolean) {
-        const displayTags: IDisplayTag[] = tags;
-        displayTags.map((displayTag: IDisplayTag): IDisplayTag => {
-            if (displayTag.id === tag.id) {
-                displayTag.checked = checked;
-                console.log("Set " + displayTag.name + " to " + checked);
-            }
-            return displayTag;
-        });
-        setTags(displayTags);
-    }
+    const [filters, setFilters] = useState<IDisplayTag[]>([]);
 
     const onPageChange = (event: { first: SetStateAction<number>; rows: SetStateAction<number>; }) => {
         setOffset(event.first);
         setPerPage(event.rows);
+    };
+
+    /**
+     * Only enabled filters
+     * @param filters
+     */
+    const onFiltersChanged = (filters: IDisplayTag[]) => {
+
     };
 
     useEffect(() => {
@@ -77,40 +72,13 @@ export default function ProductListPage(): ReactElement {
         }
     }, [offset, perPage, productListSortKey]);
 
-    useEffect(() => {
-        const tags$: Subscription = getTags().subscribe({
-            next: (results: ITag[]) => {
-                const displayTags: IDisplayTag[] = [];
-                results.map((tag: ITag) => {
-                    displayTags.push({...tag, checked: false});
-                });
-                setTags(displayTags);
-            },
-            error: (err) => {
-                if (toast.current) {
-                    toast.current.show({
-                        severity: 'error',
-                        summary: 'Error',
-                        detail: 'Error loading filters: ' + err,
-                        life: 3000,
-                    })
-                }
-            }
-        });
-        return () => {
-            tags$.unsubscribe();
-        }
-    }, []);
-
     return (
         <>
             <div className="flex gap-4">
                 <menu className="w-[205px]">
-                    {tags.length > 0 ? (
-                        <Card title="Filters">
-                            <ProductFilterList tags={tags} toggleFilter={toggleFilter}/>
-                        </Card>
-                    ) : "Loading tags..."}
+                    <Card title="Filters">
+                        {<ProductFilterList onFiltersChanged={onFiltersChanged} toast={toast}/>}
+                    </Card>
                 </menu>
 
                 <section className="w-full">
