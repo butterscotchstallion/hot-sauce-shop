c493019be086854c240b69839bf59b16f1d11c5b
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-03-27 13:42:44 -0400

    Adds user admin role

--- SQL/UserRolesAndPermissions.sql
+++ SQL/UserRolesAndPermissions.sql
@@ -1,4 +1,5 @@
-INSERT INTO user_roles(name, slug) VALUES('Admin', 'admin');
+INSERT INTO roles(name, slug) VALUES('Admin', 'admin');
+INSERT INTO roles(name, slug) VALUES('User Admin', 'user-admin');
 
 INSERT INTO user_permissions(name, slug) VALUES('Create User', 'create-user');
 INSERT INTO user_permissions(name, slug) VALUES('Read User', 'read-user');
@@ -6,7 +7,16 @@ INSERT INTO user_permissions(name, slug) VALUES('Update User', 'update-user');
 INSERT INTO user_permissions(name, slug) VALUES('Delete User', 'create-user');
 
 -- Associate permissions with role
-INSERT INTO user_roles_permissions(role_id, permission_id) VALUES(1, 1);
-INSERT INTO user_roles_permissions(role_id, permission_id) VALUES(1, 2);
-INSERT INTO user_roles_permissions(role_id, permission_id) VALUES(1, 3);
-INSERT INTO user_roles_permissions(role_id, permission_id) VALUES(1, 4);
+
+-- Admin
+INSERT INTO roles_permissions(role_id, permission_id) VALUES(1, 1);
+INSERT INTO roles_permissions(role_id, permission_id) VALUES(1, 2);
+INSERT INTO roles_permissions(role_id, permission_id) VALUES(1, 3);
+INSERT INTO roles_permissions(role_id, permission_id) VALUES(1, 4);
+
+-- User Admin
+select * from roles_permissions;
+INSERT INTO roles_permissions(role_id, permission_id) VALUES(2, 1);
+INSERT INTO roles_permissions(role_id, permission_id) VALUES(2, 2);
+INSERT INTO roles_permissions(role_id, permission_id) VALUES(2, 3);
+INSERT INTO roles_permissions(role_id, permission_id) VALUES(2, 4);
