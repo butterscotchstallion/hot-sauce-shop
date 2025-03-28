6daea0c994f077d85482e301e4a7285696c18cd6
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-03-28 16:13:48 -0400

    Adds some user details

--- ui/src/app/components/Admin/AdminUserDetail.tsx
+++ ui/src/app/components/Admin/AdminUserDetail.tsx
@@ -9,7 +9,20 @@ export function AdminUserDetail(props: IAdminUserFormProps) {
 
     return (
         <>
-            <h1 className="text-2xl font-bold">{props.user.username}</h1>
+            <div className={"flex flex-row gap-4"}>
+                <h1 className="text-2xl font-bold">{props.user.username}</h1>
+
+                <section className="flex gap-4">
+                    <aside className={"w-1/3"}>
+                        user avatar
+                    </aside>
+                    <div className={"w-2/3"}>
+                        <ul>
+                            <li>Created: {new Date(props.user.createdAt).toLocaleDateString()}</li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
         </>
     )
 }
