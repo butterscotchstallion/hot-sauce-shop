6f8bdfd1ff3297f728feca83dce3ad0be1fa371b
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-10-29 20:48:00 -0400

    Implements user level and experience functions
    - Updates test

--- lib/userlevel.go
+++ lib/userlevel.go
@@ -1,13 +1,73 @@
 package lib
 
+import (
+	"context"
+	"errors"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
 const MaxLevel = 25
 const ActivitiesRequiredPerLevel = 12
 const CommentExperience = 100
 
+const (
+	ActivityComment = iota
+	ActivityPost
+	ActivityImagePost
+)
+
+func AddExperienceToUserId(dbPool *pgxpool.Pool, experience int, userId int) error {
+	const query = `
+		INSERT INTO user_experience (user_id, experience, updated_at) VALUES ($1, $2)
+		ON CONFLICT(user_id)
+		    DO UPDATE SET experience = experience + $2, updated_at = NOW()
+	`
+	_, updateErr := dbPool.Exec(context.Background(), query, userId, experience)
+	if updateErr != nil {
+		return updateErr
+	}
+	return nil
+}
+
+func AddCommentExperienceToUser(dbPool *pgxpool.Pool, userId int) error {
+	activityTypeExperienceMap := GetActivityTypeExperienceMap()
+	return AddExperienceToUserId(dbPool, activityTypeExperienceMap[ActivityComment], userId)
+}
+
+func AddPostExperienceToUser(dbPool *pgxpool.Pool, userId int) error {
+	activityTypeExperienceMap := GetActivityTypeExperienceMap()
+	return AddExperienceToUserId(dbPool, activityTypeExperienceMap[ActivityPost], userId)
+}
+
+func AddImagePostExperienceToUser(dbPool *pgxpool.Pool, userId int) error {
+	activityTypeExperienceMap := GetActivityTypeExperienceMap()
+	return AddExperienceToUserId(dbPool, activityTypeExperienceMap[ActivityImagePost], userId)
+}
+
+func GetActivityTypeExperienceMap() map[int]int {
+	activityTypeExperienceMap := make(map[int]int)
+	activityTypeExperienceMap[ActivityComment] = 100
+	activityTypeExperienceMap[ActivityPost] = 150
+	activityTypeExperienceMap[ActivityImagePost] = 200
+	return activityTypeExperienceMap
+}
+
+func GetExperienceByActivityType(activityType int) (int, error) {
+	activityTypeExperienceMap := GetActivityTypeExperienceMap()
+	val, ok := activityTypeExperienceMap[activityType]
+	if !ok {
+		return 0, errors.New("activity type not found")
+	}
+	return val, nil
+}
+
 func GetLevelExperienceMap() map[int]int {
 	userLevels := make(map[int]int)
 	for level := range MaxLevel {
-		userLevels[level] = CommentExperience * ActivitiesRequiredPerLevel
+		if level >= 1 {
+			userLevels[level] = (CommentExperience * ActivitiesRequiredPerLevel) * level
+		}
 	}
 	return userLevels
 }
--- lib/userlevel_test.go
+++ lib/userlevel_test.go
@@ -4,9 +4,8 @@ import "testing"
 
 func TestGetLevelByExperience(t *testing.T) {
 	expectedLevel := 2
-	experience := (CommentExperience * ActivitiesRequiredPerLevel) * 2
+	experience := (CommentExperience * ActivitiesRequiredPerLevel) * expectedLevel
 	actualLevel := GetUserLevelByExperience(experience)
-
 	if actualLevel != expectedLevel {
 		t.Fatalf("GetLevelByExperience() expected %d, got %d", expectedLevel, actualLevel)
 	}
