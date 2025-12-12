06c495a7cb764c89157ff5ea4b7a057e8c67aa51
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-12-11 22:15:15 -0500

    Fixes wrapping when there are only a couple chat windows

--- ui/src/app/components/Chat/ChatArea.tsx
+++ ui/src/app/components/Chat/ChatArea.tsx
@@ -4,8 +4,8 @@ export function ChatArea({children}) {
     return (
         <>
             <section
-                className="fixed bottom-0 right-0 min-h-[350px] flex flex-wrap align-bottom gap-2 m-2 justify-end">
-                <div className="w-3/4 flex gap-2 pl-4 pr-4">
+                className="fixed w-full bottom-0 right-0 min-h-[350px] flex flex-wrap align-bottom gap-2 m-2 justify-end">
+                <div className="w-3/4 flex gap-2 pl-4 pr-4 justify-end">
                     {children}
                 </div>
                 <ChatBuddyList/>
